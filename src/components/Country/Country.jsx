014029961090716346cf2fcc0d2dd065e5ac5513
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-03-17 02:16:29 +0600

    added country btn

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
-    const {name, flags, population, area} = country;
+const Country = ({country, handleVisitedCountry, handleVisitedFlags}) => {
+    const {name, flags, population, area, cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        setVisited(!visited);
+    }
+
+
+
     return (
-        <div className='country'>
-            <h3>Name: {name?.common} </h3>
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{color: visited ? 'Purple' : 'Black'}}>Name: {name?.common} </h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark visited</button>
+            <br />
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag</button>
+            <br />
+            {visited ? 'Already Visited' :'Do you want to visit?' } <br />
+            <button onClick={handleVisited}>{visited ? 'Reset' : 'Visit'}</button>
         </div>
     );
 };
