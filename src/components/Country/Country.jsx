33fc58d43bb01bce5ff8c7de830e43f176f71a32
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-03-17 03:36:09 +0600

    added Country data

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
+import CountryDetail from '../CountryDetail/CountryDetail';
 const Country = ({country, handleVisitedCountry, handleVisitedFlags}) => {
     const {name, flags, population, area, cca3} = country;
 
@@ -24,6 +25,8 @@ const Country = ({country, handleVisitedCountry, handleVisitedFlags}) => {
             <br />
             {visited ? 'Already Visited' :'Do you want to visit?' } <br />
             <button onClick={handleVisited}>{visited ? 'Reset' : 'Visit'}</button>
+            <hr />
+            <CountryDetail country={country} handleVisitedCountry={handleVisitedCountry} handleVisitedFlags={handleVisitedFlags}></CountryDetail>
         </div>
     );
 };
